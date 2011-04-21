c2126ae1c6c702472a6f56ca8f4b7e80cafb99c2
Dominic Cerminara <dompkt2013@gmail.com>
2011-04-21 10:04:12 -0400

    model changes

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,6 +1,6 @@
 class Host < ActiveRecord::Base
   # new columns need to be added here to be writable through mass assignment
-  attr_accessible :username, :email, :password, :password_confirmation
+  attr_accessible :first_name, :last_name, :username, :email, :password, :password_confirmation
 
   attr_accessor :password
   before_save :prepare_password
