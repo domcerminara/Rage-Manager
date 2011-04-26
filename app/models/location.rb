53d6ee0a078a2363b4e6057c6715d6363802ff04
Dominic Cerminara <dompkt2013@gmail.com>
2011-04-26 14:23:08 -0400

    working

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,7 +2,7 @@ class Location < ActiveRecord::Base
   attr_accessible :host_id, :name, :street, :city, :state, :zip, :latitude, :longitude
 
   validates_presence_of :name, :street, :city, :state, :zip, :latitude, :longitude
-  validates_unqiqueness_of :name
+  validates_uniqueness_of :name
   validates_numericality_of :zip, :latitude, :longitude
   validates_length_of :zip, :is => 5
 
