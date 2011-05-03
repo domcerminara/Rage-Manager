746e68063b2f91e8db2e77c6e0a5e6f7684bc645
Dominic Cerminara <dompkt2013@gmail.com>
2011-05-02 20:53:44 -0400

    10/11

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,9 +2,9 @@ class Invitation < ActiveRecord::Base
   attr_accessible :party_id, :guest_id, :invite_code, :expected_attendees, :actual_attendees
 
   #Validations
-  validates_presence_of :party_id, :guest_id, :expected_attendees
   validates_numericality_of :expected_attendees, :only_integer => true, :greater_than => 0
-  validates_numericality_of :party_id, :guest_id
+  validates_numericality_of :party_id
+  validates_numericality_of :guest_id
    
   #Relationships
   has_many :gifts
